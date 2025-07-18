384af573305bdf4e7c1ddaac72cfdffa7d16053c
Veerinder Kaur <veerinderkaur91@gmail.com>
2025-07-18 10:15:34 +0530

    Add files via upload

---
+++ E-commerce/assets/js/first.js
@@ -0,0 +1,9 @@
+ $(document).ready(function()
+            {
+          $('.mean-toggle').click(function()
+             {
+              $('.mean-toggle').toggleClass('active')
+              $('nav').toggleClass('active')
+            })
+
+              })
